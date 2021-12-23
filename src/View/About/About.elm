7c86f98fbca7aa016b8e38bd77323a13cd7eb5cc
Alexander Binaei <alex.binaei@carpe.io>
2021-12-23 15:14:26 -0300

    web3 hyperlink

--- src/View/About/About.elm
+++ src/View/About/About.elm
@@ -52,10 +52,21 @@ body =
             ]
             [ Html.div
                 []
-                [ Html.text
-                    """
-                    Collaborative effort of Musicians, Producers, Designers, and Web3 Developers
-                    """
+                [ Html.p
+                    []
+                    [ Html.text
+                        """
+                        Collaborative effort of Musicians, Producers, Designers, and
+                        """
+                    , Html.a
+                        [ class "has-sky-blue-text"
+                        , href "https://en.wikipedia.org/wiki/Web3"
+                        , target "_blank"
+                        ]
+                        [ Html.text "Web3 "
+                        ]
+                    , Html.text "Developers"
+                    ]
                 ]
             , Html.div
                 []
