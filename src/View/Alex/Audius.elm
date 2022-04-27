03612ca698944fbfd10fc6324bcc812e3ee9387f
Alexander Binaei <alex.binaei@carpe.io>
2022-04-27 16:12:33 -0300

    cover letter close

--- src/View/Alex/Audius.elm
+++ src/View/Alex/Audius.elm
@@ -54,24 +54,26 @@ body =
                 ]
                 [ Html.text
                     """Monetizing the project is my responsibility. Primarily because I am the principal investor.
-                    My goal has never been to make a bunch of money. If that was the case I wouldn't have pursued
-                    music.
+                    My goal has never been to make a bunch of money. If that was the case I wouldn't be producing
+                    jazz musicians.
                     """
                 ]
             , Html.div
                 [ class "mb-3"
                 ]
                 [ Html.text
-                    """Being that I'm a software engineer, converting on digital sales has always stood out to me.
-                    Exclusivity is what I'm after right now. Like a limited vinyl press, but digital.
+                    """But I've seen indie jazz musicians build sustainable businesses. The business model is to release
+                    often. Distribute most of it for free via streaming platforms. Reserve some of it exclusively for
+                    purchase. This is typically manifested as limited vinyl pressings. My ambition is to reproduce this
+                    in the digital world.
                     """
                 ]
             , Html.div
-                [
+                [ class "mb-3"
                 ]
                 [ Html.text
-                    """So this web3 thing seemed like an obvious fit. But the existing NFT space didn't fit my use case.
-                    Particularly because standard protocols like
+                    """This is where the web3 stuff comes into play. After some research I felt that the existing NFT
+                     space didn't fit my use case. Particularly because standard protocols like
                     """
                 , Html.a
                     [ class "has-sky-blue-text"
@@ -81,7 +83,7 @@ body =
                     [ Html.text "metaplex"
                     ]
                 , Html.text
-                    """publish unencrypted data to datastores like 
+                    """ publish unencrypted data to datastores like
                     """
                 , Html.a
                     [ class "has-sky-blue-text"
@@ -91,6 +93,58 @@ body =
                     [ Html.text "arweave"
                     ]
                 , Html.text "."
+                ]
+            , Html.div
+                [ class "mb-3"
+                ]
+                [ Html.text
+                    """So I built my own
+                    """
+                , Html.a
+                    [ class "has-sky-blue-text"
+                    , href "https://store.somos.world/#/buy"
+                    , target "_target"
+                    ]
+                    [ Html.text "solana dapp"
+                    ]
+                , Html.text
+                    """
+                    . It provides the "right-to-download" of exclusive audio/video from our label. Complete with a
+                    secondary market. We're also using this program to sell tickets to our shows/events.
+                    """
+                ]
+            , Html.div
+                [ class "mb-3"
+                ]
+                [ Html.text
+                    """& while this dapp works great for us right now, it's obvious to me this is just a start. A proof
+                    of concept really.
+                    """
+                ]
+            , Html.div
+                [ class "mb-3"
+                ]
+                [ Html.text "The "
+                , Html.a
+                    [ class "has-sky-blue-text"
+                    , href "https://whitepaper.audius.co/AudiusWhitepaper.pdf"
+                    , target "_target"
+                    ]
+                    [ Html.text "Audius Whitepaper"
+                    ]
+                , Html.text
+                    """ details a much larger vision. My ambition of exclusive sales is specified as Content Permissioning
+                    in section 3.3. The decentralized network of Content & Discovery nodes promotes the community as
+                    collective benefactors.
+                    """
+                ]
+            , Html.div
+                []
+                [ Html.text
+                    """I feel compelled to contribute to this project. Not only do I bring day-one-production-ready
+                    technical experience. I'm walking every day in this music industry & want nothing more than to
+                    empower the artists who have given me so much joy in this life.
+                    """
                 ]
             ]
         ]
