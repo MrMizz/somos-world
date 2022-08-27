e3c5f9c77d5d77fa77ccb557fb570a0cbbd407d9
alex <aabinaei@gmail.com>
2022-08-26 17:53:49 -0700

    releases tlc

--- src/View/Description/EP01.elm
+++ src/View/Description/EP01.elm
@@ -2,22 +2,28 @@ module View.Description.EP01 exposing (body)
 
 import Html exposing (Html)
 import Html.Attributes exposing (class)
+import Model.State as State
 import Msg.Msg exposing (Msg)
 
 
 body : Html Msg
 body =
     Html.div
-        [ class "container has-font-1"
+        [ class "container has-font-1 has-border-3 pt-2 pb-6 px-3"
         ]
         [ Html.p
             []
             [ Html.div
                 [ class "has-space-bottom-2"
                 ]
-                [ Html.text
-                    """
-                    CDMX EP was recorded in 3 days in Mexico City.
+                [ Html.a
+                    [ class "has-sky-blue-text"
+                    , State.href <| State.Releases
+                    ]
+                    [ Html.text "CDMX EP"
+                    ]
+                , Html.text
+                    """ was recorded in 3 days in Mexico City.
                     """
                 ]
             , Html.div
--- src/View/Gallery/EP01.elm
+++ src/View/Gallery/EP01.elm
@@ -2,29 +2,44 @@ module View.Gallery.EP01 exposing (body)
 
 import Html exposing (Html)
 import Html.Attributes exposing (class, src)
+import Model.State as State
 import Msg.Msg exposing (Msg)
 
 
 body : Html Msg
 body =
     Html.div
-        [ class "columns is-mobile is-multiline"
+        [ class "has-border-3 pt-2 pb-6 px-3"
         ]
-        [ column "01"
-        , column "02"
-        , column "03"
-        , column "04"
-        , column "05"
-        , column "06"
-        , column "07"
-        , column "08"
-        , column "09"
-        , column "10"
-        , column "11"
-        , column "12"
-        , column "13"
-        , column "14"
-        , column "15"
+        [ Html.div
+            [ class "mb-2 has-border-1 is-family-secondary"
+            ]
+            [ Html.a
+                [ class "has-sky-blue-text"
+                , State.href <| State.Releases
+                ]
+                [ Html.text "CDMX EP"
+                ]
+            ]
+        , Html.div
+            [ class "columns is-mobile is-multiline"
+            ]
+            [ column "01"
+            , column "02"
+            , column "03"
+            , column "04"
+            , column "05"
+            , column "06"
+            , column "07"
+            , column "08"
+            , column "09"
+            , column "10"
+            , column "11"
+            , column "12"
+            , column "13"
+            , column "14"
+            , column "15"
+            ]
         ]
 
 
