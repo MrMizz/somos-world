c60798e58eef7f377cfbb9ae829247f4b71d8c90
alex <aabinaei@gmail.com>
2022-09-10 17:59:36 -0700

    dap slide-deck

--- src/View/Alex/Dap.elm
+++ src/View/Alex/Dap.elm
@@ -12,7 +12,7 @@ body =
         ]
         [ Html.a
             [ class "has-sky-blue-text"
-            , href "https://docs.google.com/presentation/d/1pAt8oMtQpK3nO45iCbyo0SoC9I40EpmszKq46q5lufQ/edit#slide=id.gc6f73a04f_0_9"
+            , href "https://docs.google.com/presentation/d/1pAt8oMtQpK3nO45iCbyo0SoC9I40EpmszKq46q5lufQ/edit?usp=sharing"
             , target "_blank"
             ]
             [ Html.text "slide-deck"
