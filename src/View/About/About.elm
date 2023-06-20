e046ba3a89e6ddc411f2fc8741bd91b5eaf601ff
alex <aabinaei@gmail.com>
2023-06-20 15:24:54 -0700

    added to bio

--- src/View/About/About.elm
+++ src/View/About/About.elm
@@ -2,6 +2,7 @@ module View.About.About exposing (body)
 
 import Html exposing (Html)
 import Html.Attributes exposing (class, href, target)
+import Model.State as State
 import Msg.Msg exposing (Msg)
 
 
@@ -37,7 +38,7 @@ body =
                     ]
                 ]
             , Html.p
-                [ class "has-font-1 has-opacity"
+                [ class "has-font-1 has-opacity has-space-bottom-2"
                 ]
                 [ Html.div
                     [ class "mb-2"
@@ -64,6 +65,77 @@ body =
                         """
                         Currently based in São Paulo, Brasil with roots in Los Angeles, CA
                         """
+                    ]
+                ]
+            , Html.p
+                [ class "has-font-1 has-opacity has-space-bottom-2"
+                ]
+                [ Html.div
+                    [ class "mb-2"
+                    ]
+                    [ Html.p
+                        []
+                        [ Html.text
+                            """
+                            Our debut 
+                            """
+                        , Html.a
+                            [ class "has-sky-blue-text"
+                            , State.href State.Links
+                            ]
+                            [ Html.text "CDMX EP"
+                            ]
+                        , Html.text
+                            """ was recorded in 3 days in Mexico City. All composition & production was done "on the spot". 
+                            """
+                        ]
+                    ]
+                , Html.div
+                    []
+                    [ Html.text
+                        """
+                        What became from that approach is an honest sound.
+                        """
+                    ]
+                ]
+            , Html.p
+                [ class "has-font-1 has-opacity"
+                ]
+                [ Html.div
+                    [ class "mb-2"
+                    ]
+                    [ Html.p
+                        []
+                        [ Html.div
+                            [ class "mb-2"
+                            ]
+                            [ Html.text
+                                """Following the EP we packed our bags for São Paulo where we recorded a full length album. Again, mostly written & produced in the studio with no demos. 
+                            """
+                            ]
+                        , Html.div
+                            [ class "mb-2"
+                            ]
+                            [ Html.text
+                                """Lots of synths & vintage gear was involved. Inspired by the 80s with a healthy dose of Brasil's samba sound.
+                            """
+                            ]
+                        , Html.div
+                            []
+                            [ Html.text
+                                """The 
+                            """
+                            , Html.a
+                                [ class "has-sky-blue-text"
+                                , State.href State.Links
+                                ]
+                                [ Html.text "São Paulo LP"
+                                ]
+                            , Html.text
+                                """ is available now for streaming.
+                        """
+                            ]
+                        ]
                     ]
                 ]
             ]
